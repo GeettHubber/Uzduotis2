fea929ee5b2bd50186c48b3e23f1bc99033f4514
cinis77 <cinis.dwarnius.minis@gmail.com>
2020-06-29 21:25:02 +0300

    minor changes

---
+++ GitUzduotis/GitUzduotis/A.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GitUzduotis
+{
+    internal class A : IHero, IVilain
+    {
+        private string UserName { get; set; }
+        private int Level { get; set; }
+        string IHero.UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        int IHero.Level { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IVilain.UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        int IVilain.Level { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string SkaitytiZodi()
+        {
+            UserName = Console.ReadLine();
+            return UserName;
+        }
+
+        public int SkaitytiSkaiciu()
+        {
+            Level = Convert.ToInt32(Console.ReadLine());
+            return Level;
+        }
+
+        double IHero.AddPower(double power)
+        {
+            return Level / 2;
+        }
+
+        void IHero.Strength()
+        {
+        }
+
+        public void Strength()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GitUzduotis/GitUzduotis/Program.cs
+++ GitUzduotis/GitUzduotis/Program.cs
@@ -6,56 +6,14 @@ using System.Threading.Tasks;
 
 namespace GitUzduotis
 {
-    public interface IHero
+    internal class B : IVilain
     {
-        string UserName { get; set; }
-        int Level { get; set; }
-
-        void Strength();
-        double AddPower(double power);
-    }
-
-    interface IVilain
-    {
-        
-        string UserName { get; set; }
-        int Level { get; set; }
-
-        void Strength();
-    }
-
-    class A : IHero
-    {
-        string UserName { get; set; }
-        int Level { get; set; }
-        public string SkaitytiZodi()
-        {
-            UserName = Console.ReadLine();
-                return UserName;
-        }
-        public int SkaitytiSkaiciu()
-        {
-            Level = Convert.ToInt32(Console.ReadLine());
-            return Level;
-        }
-        double IHero.AddPower(double power)
-        {
-            return Level / 2;
-        }
-        void IHero.Strength()
-        {
-
-        }
-    }
-
-    class B : IVilain
-    {
-        string UserName { get;  set; }
+        private string UserName { get; set; }
         string IVilain.UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        int Level { get; set; }
+        private int Level { get; set; }
         int IVilain.Level { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-        public void Duomenys ( string username, int level)
+        public void Duomenys(string username, int level)
         {
             UserName = username;
             Level = level;
@@ -63,13 +21,12 @@ namespace GitUzduotis
 
         void IVilain.Strength()
         {
-
         }
     }
 
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
